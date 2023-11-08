54da531fa608832d989b83d5c002b510737334d9
Dmitri Gvozdev <dmitrigvozdev94@gmail.com>
2023-11-08 05:52:22 -0500

    Figure out how classes work sorta

--- DefaultAzureCredentials.php
+++ DefaultAzureCredentials.php
@@ -15,6 +15,22 @@ class DefaultAzureCredentials {
         $this->checkAvailableCredentials();
     }
 
+    public function getAccessToken() {
+        //TODO This should go to the correct endpoint based on what is requested, detect storage, pass in the storage resource, etc
+        $resource = 'https://storage.azure.com/';
+        if($this->authType == AuthType::AzLogin){
+            $command = "az account get-access-token --resource $resource --output json";
+            $output = shell_exec($command);
+            $tokenData = json_decode($output, true);
+            if (isset($tokenData['accessToken'])) {
+                return $tokenData['accessToken'];
+            }
+            else {
+                throw new Exception("Failed to grab Access token from AZLOGIN");
+            }
+        }
+    }
+
     private function checkAvailableCredentials() {
         if($this->hasServicePrincipal()) {
             echo "Detected Service Principal";
@@ -52,7 +68,12 @@ class DefaultAzureCredentials {
     }
 }
 
-
-$defaultCreds = new DefaultAzureCredentials();
+## Replicating python if __main__
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
+    $defaultCreds = new DefaultAzureCredentials();
+    $accessToken = $defaultCreds->getAccessToken();
+    
+    echo $accessToken;
+}
 
 ?>
--- test.php
+++ test.php
@@ -1,49 +1,8 @@
 <?php
 
-function getAccessToken($resource) {
-    // Check for environment variable credentials first
-    if (getenv('AZURE_CLIENT_ID') && getenv('AZURE_CLIENT_SECRET') && getenv('AZURE_TENANT_ID')) {
-        // If available, use them to acquire an access token (see previous examples for implementation)
-        // ...
+require_once 'DefaultAzureCredentials.php';
 
-    } elseif (getenv('IDENTITY_ENDPOINT')) {
-        // If the environment variables are not available, try to get a token from the managed identity
-        $tokenUrl = getenv('IDENTITY_ENDPOINT') . '?api-version=2019-08-01&resource=' . urlencode($resource);
-        $secret = getenv('IDENTITY_HEADER');
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $tokenUrl);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-IDENTITY-HEADER: ' . $secret]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        $responseData = json_decode($response, true);
-        if (isset($responseData['access_token'])) {
-            return $responseData['access_token'];
-        }
-
-    } else {
-        // If managed identity is not available, try to get the access token from Azure CLI cache
-        $command = 'az account get-access-token --resource ' . escapeshellarg($resource) . ' --output json';
-        $output = shell_exec($command);
-        $tokenData = json_decode($output, true);
-        if (isset($tokenData['accessToken'])) {
-            return $tokenData['accessToken'];
-        }
-    }
-
-    throw new Exception('Unable to acquire an access token.');
-}
-
-// Example usage
-try {
-    $resource = 'https://storage.azure.com/';
-    $accessToken = getAccessToken($resource);
-    //echo "Access Token: " . $accessToken . "\n";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage() . "\n";
-}
+$defaultCreds = new DefaultAzureCredentials();
 
 
 
@@ -59,6 +18,7 @@ $blobName = getenv('BLOB_NAME');
 $blobUri = "https://$storageAccount.blob.core.windows.net/$containerName/$blobName";
 
 // Create the HTTP headers for the request with the access token
+$accessToken = $defaultCreds->getAccessToken();
 $headers = [
     "Authorization: Bearer $accessToken",
     "x-ms-version: 2020-04-08", // use the latest version appropriate for your scenario
